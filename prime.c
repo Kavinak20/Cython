db0b119e34ba0bde3fb06b3906b44de076619371
Kavinak20 <30405524+Kavinak20@users.noreply.github.com>
2017-08-12 16:23:14 +0530

    Update and rename 13.c to prime.c

--- prime.c
+++ prime.c
@@ -2,13 +2,10 @@
 int main()
 {
     int n, i, flag = 0;
-
     printf("Enter a positive integer: ");
     scanf("%d",&n);
-
     for(i=2; i<=n/2; ++i)
     {
-        // condition for nonprime number
         if(n%i==0)
         {
             flag=1;
@@ -20,6 +17,5 @@ int main()
         printf("%d is a prime number.",n);
     else
         printf("%d is not a prime number.",n);
-    
     return 0;
 }
