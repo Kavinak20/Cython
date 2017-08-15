a42904382dbdfbec81169fdbf68deaad97aafcfb
Kavinak20 <30405524+Kavinak20@users.noreply.github.com>
2017-08-15 10:10:46 -0500

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+int main(void)
+{ 
+char a='Q',b; 
+for(;;) 
+{ 
+      printf("Enter a character"); 
+      scanf("%c",&b); 
+      if(b=='Q')
+      break; 
+      } 
+	return 0; 
+  }
